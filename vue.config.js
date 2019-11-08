3c8b441d0cebac066e3b6b14f58a2f02e009539a
rige <jrw19960116@gmail.com>
2019-11-08 16:09:17 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,14 @@
+const DEV_PATH = "/";
+const PROD_PATH = "/rms";
+
+function isProduct() {
+    return process.env.NODE_ENV === 'production';
+}
+
 module.exports = {
+    publicPath: isProduct() ? PROD_PATH : DEV_PATH,
     devServer: {
         port: 8000,
         proxy: 'http://localhost:9000'
-    },
-    css: {
-        loaderOptions: {
-            less: {
-                modifyVars: {
-                    'primary-color': '#1DA57A',
-                    'link-color': '#1DA57A',
-                    'border-radius-base': '2px',
-                },
-                javascriptEnabled: true
-            }
-        }
     }
 };
